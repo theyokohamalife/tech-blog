a9ee857a16b95b0c84ecbe6a535ac177e4cee7a4
LaShawn Toyoda <lts001@gmail.com>
2021-02-15 16:52:42 +0900

    first commit

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+  <article>
+      <p>Post last updated: {{ formatDate(article.createdAt) }} </p>
+    <nuxt-content :document="article" />
+  </article>
+</template>
+
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const article = await $content('articles', params.slug).fetch()
+
+      return { article }
+    },
+    methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString('en', options)
+    }
+ }
+
+  }
+</script>
